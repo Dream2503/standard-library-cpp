30922c005c8446ffea0fd2d8e55ef773f6d676e7
Dream2503 <swapnarajmohanty2503.com>
2025-04-01 19:40:45 +0530

    updated, no need of the child template argument anymore

--- iterator.hpp
+++ iterator.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace Dream {
-    template<typename T, typename Child>
+    template<typename T>
     class iterator {
     protected:
         T *ptr_ = nullptr;
@@ -10,9 +10,8 @@ namespace Dream {
         constexpr iterator() noexcept = default;
         constexpr iterator(const T *value) noexcept : ptr_(const_cast<T *>(value)) {}
         constexpr iterator(const iterator &itr) noexcept : ptr_(itr.ptr_) {}
-
-        constexpr Child operator+(const size_t offset) const noexcept { return ptr_ + offset; }
-        constexpr Child operator-(const size_t offset) const noexcept { return ptr_ - offset; }
+        constexpr auto operator+(const size_t offset) const noexcept { return ptr_ + offset; }
+        constexpr auto operator-(const size_t offset) const noexcept { return ptr_ - offset; }
         constexpr size_t operator-(const iterator &other) const noexcept { return ptr_ - other.ptr_; }
         constexpr bool operator==(const iterator &itr) const noexcept { return ptr_ == itr.ptr_; }
         constexpr bool operator!=(const iterator &itr) const noexcept { return ptr_ != itr.ptr_; }
@@ -20,102 +19,102 @@ namespace Dream {
         constexpr operator const void *() const noexcept { return static_cast<const void *>(ptr_); }
         constexpr operator const T *() const noexcept { return static_cast<const T *>(ptr_); }
 
-        constexpr Child &operator=(const T value) noexcept {
+        constexpr iterator &operator=(const T value) noexcept {
             *ptr_ = value;
-            return *static_cast<Child *>(this);
+            return *this;
         }
 
-        constexpr Child &operator=(const iterator &other) noexcept {
+        constexpr iterator &operator=(const iterator &other) noexcept {
             if (this != &other) {
                 ptr_ = other.ptr_;
-                return *static_cast<Child *>(this);
+                return *this;
             }
-            return *static_cast<Child *>(this);
+            return *this;
         }
 
-        constexpr Child &operator++() noexcept {
+        constexpr iterator &operator++() noexcept {
             ++ptr_;
-            return *static_cast<Child *>(this);
+            return *this;
         }
 
-        constexpr Child operator++(int) noexcept {
-            Child temp = *static_cast<Child *>(this);
+        constexpr iterator operator++(int) noexcept {
+            iterator temp = *this;
             ++ptr_;
             return temp;
         }
 
-        constexpr Child &operator--() noexcept {
+        constexpr iterator &operator--() noexcept {
             --ptr_;
-            return *static_cast<Child *>(this);
+            return *this;
         }
 
-        constexpr Child operator--(int) noexcept {
-            Child temp = *static_cast<Child *>(this);
+        constexpr iterator operator--(int) noexcept {
+            iterator temp = *this;
             --ptr_;
             return temp;
         }
 
-        constexpr Child &operator+=(const size_t offset) noexcept {
+        constexpr iterator &operator+=(const size_t offset) noexcept {
             ptr_ += offset;
-            return *static_cast<Child *>(this);
+            return *this;
         }
 
-        constexpr Child &operator-=(const size_t offset) noexcept {
+        constexpr iterator &operator-=(const size_t offset) noexcept {
             ptr_ -= offset;
-            return *static_cast<Child *>(this);
+            return *this;
         }
     };
 
 
-    template<typename T, typename Child>
-    class reverse_iterator : public iterator<T, reverse_iterator<T, Child>> {
+    template<typename T>
+    class reverse_iterator : public iterator<T> {
     protected:
-        using iterator<T, reverse_iterator>::ptr_;
+        using iterator<T>::ptr_;
 
     public:
-        using iterator<T, reverse_iterator>::iterator;
+        using iterator<T>::iterator;
 
-        constexpr Child &operator++() noexcept {
+        constexpr reverse_iterator &operator++() noexcept {
             --ptr_;
             return *this;
         }
 
-        constexpr Child operator++(int) noexcept {
-            Child temp = *this;
+        constexpr reverse_iterator operator++(int) noexcept {
+            reverse_iterator temp = *this;
             --ptr_;
             return temp;
         }
 
-        constexpr Child &operator--() noexcept {
+        constexpr reverse_iterator &operator--() noexcept {
             ++ptr_;
             return *this;
         }
 
-        Child operator--(int) noexcept {
-            Child temp = *this;
+        reverse_iterator operator--(int) noexcept {
+            reverse_iterator temp = *this;
             ++ptr_;
             return temp;
         }
 
-        constexpr Child &operator+=(const size_t offset) noexcept {
+        constexpr reverse_iterator &operator+=(const size_t offset) noexcept {
             ptr_ -= offset;
             return *this;
         }
 
-        constexpr Child &operator-=(const size_t offset) noexcept {
+        constexpr reverse_iterator &operator-=(const size_t offset) noexcept {
             ptr_ += offset;
             return *this;
         }
     };
 
     template<typename T>
-    constexpr auto begin(T &child) -> decltype(child.begin()) {
-        return child.begin();
+    constexpr auto begin(T &itr) -> decltype(itr.begin()) {
+        return itr.begin();
     }
 
     template<typename T>
-    constexpr auto begin(const T &child) -> decltype(begin(child)) {
-        return child.begin();
+    constexpr auto begin(const T &itr) -> decltype(begin(itr)) {
+        return itr.begin();
     }
 
     template<typename T, size_t N>
@@ -124,19 +123,19 @@ namespace Dream {
     }
 
     template<typename T>
-    constexpr auto cbegin(const T &child) -> decltype(child.cbegin()) {
-        return child.cbegin();
+    constexpr auto cbegin(const T &itr) -> decltype(itr.cbegin()) {
+        return itr.cbegin();
     }
 
 
     template<typename T>
-    constexpr auto rbegin(T &child) -> decltype(child.rbegin()) {
-        return child.rbegin();
+    constexpr auto rbegin(T &itr) -> decltype(itr.rbegin()) {
+        return itr.rbegin();
     }
 
     template<typename T>
-    constexpr auto rbegin(const T &child) -> decltype(rbegin(child)) {
-        return child.rbegin();
+    constexpr auto rbegin(const T &itr) -> decltype(rbegin(itr)) {
+        return itr.rbegin();
     }
 
     template<typename T, size_t N>
@@ -145,19 +144,19 @@ namespace Dream {
     }
 
     template<typename T>
-    constexpr auto crbegin(const T &child) -> decltype(child.crbegin()) {
-        return child.crbegin();
+    constexpr auto crbegin(const T &itr) -> decltype(itr.crbegin()) {
+        return itr.crbegin();
     }
 
 
     template<typename T>
-    constexpr auto end(T &child) -> decltype(child.end()) {
-        return child.end();
+    constexpr auto end(T &itr) -> decltype(itr.end()) {
+        return itr.end();
     }
 
     template<typename T>
-    constexpr auto end(const T &child) -> decltype(end(child)) {
-        return child.end();
+    constexpr auto end(const T &itr) -> decltype(end(itr)) {
+        return itr.end();
     }
 
     template<typename T, size_t N>
@@ -166,19 +165,19 @@ namespace Dream {
     }
 
     template<typename T>
-    constexpr auto cend(const T &child) -> decltype(child.cend()) {
-        return child.cend();
+    constexpr auto cend(const T &itr) -> decltype(itr.cend()) {
+        return itr.cend();
     }
 
 
     template<typename T>
-    constexpr auto rend(T &child) -> decltype(child.rend()) {
-        return child.rend();
+    constexpr auto rend(T &itr) -> decltype(itr.rend()) {
+        return itr.rend();
     }
 
     template<typename T>
-    constexpr auto rend(const T &child) -> decltype(rend(child)) {
-        return child.rend();
+    constexpr auto rend(const T &itr) -> decltype(rend(itr)) {
+        return itr.rend();
     }
 
     template<typename T, size_t N>
@@ -187,7 +186,7 @@ namespace Dream {
     }
 
     template<typename T>
-    constexpr auto crend(const T &child) -> decltype(child.crend()) {
-        return child.crend();
+    constexpr auto crend(const T &itr) -> decltype(itr.crend()) {
+        return itr.crend();
     }
 } // namespace Dream
