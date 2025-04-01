9505350236558c7888f5a38f7e73a6289e9aa735
Dream2503 <swapnarajmohanty2503.com>
2025-04-01 19:41:37 +0530

    bugfix, assignment of nullptr after de-allocation

--- memory.hpp
+++ memory.hpp
@@ -9,13 +9,17 @@ namespace Dream {
         constexpr T *allocate(const size_t size) const noexcept { return static_cast<T *>(malloc(size * sizeof(T))); }
 
         constexpr bool reallocate(T *&ptr, const size_t size) const noexcept {
-            if (T *res = static_cast<T*>(realloc(ptr, size * sizeof(T)))) {
+            if (T *res = static_cast<T *>(realloc(ptr, size * sizeof(T)))) {
                 ptr = res;
                 return true;
             }
             return false;
         }
-        constexpr void deallocate(T *ptr) const noexcept { free(ptr); }
+
+        constexpr void deallocate(T *&ptr) const noexcept {
+            free(ptr);
+            ptr = nullptr;
+        }
         constexpr void construct(T *ptr, const T value) const noexcept { *ptr = value; }
         constexpr void destroy(T *ptr) const noexcept { ptr->~T(); }
     };
